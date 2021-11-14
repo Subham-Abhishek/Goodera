f90c21f5746a0a136a7c99e8d9d5c33465cd5a1c
Subham-Abhishek <abhi950727@gmail.com>
2021-11-14 20:27:30 +0530

    a

--- Server/src/db/conn.js
+++ Server/src/db/conn.js
@@ -4,4 +4,4 @@ mongoose.connect("mongodb+srv://Subham:Subham@goodera.3giyy.mongodb.net/goodera"
     console.log("MongoDB connection established");
 }).catch(() => {
     console.log("Something went wrong");
-})
+}) 
